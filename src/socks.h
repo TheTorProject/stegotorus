8f0aaa15938f29c44a7d1644afe9aa499775edbb
George Kadianakis <desnacked@gmail.com>
2011-02-19 14:45:51 +0100

    * We are now using uchar buffers to parse the protocol instead of structs. * Modified SOCKS functions logic to be easier to unittest.
    Thanks nickm!

--- src/socks.h
+++ src/socks.h
@@ -4,8 +4,8 @@
 typedef struct socks_state_t socks_state_t;
 struct evbuffer;
 
-int handle_socks(struct evbuffer *input, 
-                 struct evbuffer *output, void *arg);
+int handle_socks(struct evbuffer *source, 
+                 struct evbuffer *dest, void *arg);
 socks_state_t *socks_state_new(void);
 
 #define SOCKS5_VERSION         0x05
@@ -22,7 +22,19 @@ socks_state_t *socks_state_new(void);
 
 /* Minimum SOCKS packet length is 3 bytes:
    Method Negotiation Packet with 1 method */ 
-#define MIN_SOCKS_PACKET       0x03
+#define MIN_SOCKS_PACKET       3
+
+/* Size of a SOCKS request reply packet.
+   Yes, it's static. Yes, it's 10. */
+#define SIZEOF_SOCKS5_REQ_REPLY   10
+
+/* Size of the static part of a SOCKS request:
+   | version | cmd | rsv | atyp |
+       1b       1b   1b     1b
+*/
+#define SIZEOF_SOCKS5_STATIC_REQ 4
+       
+   
 
 struct socks_state_t {
   enum {
