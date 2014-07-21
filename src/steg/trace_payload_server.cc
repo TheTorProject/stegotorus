3f90de28b4312fb51798bf657eacd3e89a9a6325
Noah Rahman <selimthegrim@gmail.com>
2014-07-21 14:20:30 -0400

    last capacity call moved over

--- src/steg/trace_payload_server.cc
+++ src/steg/trace_payload_server.cc
@@ -1,4 +1,5 @@
 #include "util.h"
+#include "http_steg_mods/pdfSteg.h"
 #include "trace_payload_server.h"
 
 
@@ -242,7 +243,7 @@ TracePayloadServer::init_PDF_payload_pool(int len, int type, int minCapacity)
       // use capacityPDF() to find out the amount of data that we
       // can encode in the pdf doc 
       // cap = minCapacity+1;
-      cap = capacityPDF(msgbuf, p->length);
+      cap = PDFSteg::capacity(msgbuf, p->length);
       if (cap > minCapacity) {
 	log_debug("pdf (index %d) greater than mincapacity %d", cnt, minCapacity);
 	pl.typePayloadCap[contentType][cnt] = (cap-PDF_DELIMITER_SIZE)/2;
