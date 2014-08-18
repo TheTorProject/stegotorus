483b7a5bfb23bd806f9b2e599b6c4a1fe381edd5
Noah Rahman <selimthegrim@gmail.com>
2014-08-18 13:57:35 -0500

    Switched JS util to uint8_t *
    Switched pointer type in order to get around compile errors.

--- src/evbuf_util.cc
+++ src/evbuf_util.cc
@@ -50,7 +50,7 @@ JS_evbuffer_to_memory_block(evbuffer* scattered_buffer, uint8_t** memory_block)
 {
 
   size_t sbuflen = evbuffer_get_length(scattered_buffer);
-  unsigned int data_len = 0;
+  size_t data_len = 0;
   int nv = evbuffer_peek(scattered_buffer, sbuflen, NULL, NULL, 0);
   evbuffer_iovec* iv = (evbuffer_iovec *)xzalloc(sizeof(struct evbuffer_iovec) * nv);
 
@@ -66,14 +66,14 @@ JS_evbuffer_to_memory_block(evbuffer* scattered_buffer, uint8_t** memory_block)
   
   size_t cnt = 0;
   for (int i = 0; i < nv; i++) {
-    const unsigned char *p = (const unsigned char *)iv[i].iov_base;
-    const unsigned char *limit = p + iv[i].iov_len;
-    char c;
+    const uint8_t *p = (const unsigned char *)iv[i].iov_base;
+    const uint8_t *limit = p + iv[i].iov_len;
+    uint8_t c;
     while (p < limit && cnt < sbuflen) {
-      //(*memory_block)[cnt++] = *p++;
+      //(*memory_block)[cnt++] = *p++; may need to move parentheses around a bit
       c = *p++;
-      *memory_block[data_len] = "0123456789abcdef"[(c & 0xF0) >> 4];
-      *memory_block[data_len+1] = "0123456789abcdef"[(c & 0x0F) >> 0];
+      *(memory_block)[data_len] = "0123456789abcdef"[(c & 0xF0) >> 4]; //does this need to change to 8, I don't think so, just hex encoding, this function is present elsewhere too
+      *(memory_block)[data_len+1] = "0123456789abcdef"[(c & 0x0F) >> 0];
       data_len += 2;
       cnt++;
     }
